c0a3723b16d00c2c9b5471c1b4bed592fe6f50cb
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2019-01-02 21:50:53 +0100

    removed encryption as it rendered searching impossible :/ facepalm...

--- app.py
+++ app.py
@@ -2,70 +2,12 @@ import uuid
 import json
 import os
 from datetime import datetime
-from Crypto.Cipher import AES
-import base64
-
 from flask import Flask, render_template, jsonify, request, send_from_directory
 
 
 app = Flask(__name__)
 
 db_provider = os.getenv("DB_PROVIDER", "file")
-
-secret_key = os.getenv("CRYPTO_SECRET_KEY", False)
-
-if secret_key:
-    cipher = AES.new(secret_key, AES.MODE_ECB)
-
-
-def string_padding(text):
-    extra = len(text) % 16
-    if extra > 0:
-        text = text + (" " * (16 - extra))
-
-    return text
-
-
-def string_encode(text):
-    if secret_key:
-        try:
-            return base64.b64encode(cipher.encrypt(string_padding(text))).strip().decode('utf8')
-        except Exception as e:
-            print(e)
-
-    return text
-
-
-def string_decode(text):
-    if secret_key:
-        try:
-            return cipher.decrypt(base64.b64decode(text)).strip().decode('utf8')
-        except Exception as e:
-            print(e)
-
-    return text
-
-
-def note_encode(note):
-    note["encoded"] = True
-    note["title"] = string_encode(note["title"])
-    note["content"] = string_encode(note["content"])
-
-    if "tags" in note:
-        note["tags"] = list(map(lambda tag: string_encode(tag), note["tags"]))
-
-    return note
-
-
-def note_decode(note):
-    note["title"] = string_decode(note["title"])
-    note["content"] = string_decode(note["content"])
-
-    if "tags" in note:
-        note["tags"] = list(map(lambda tag: string_decode(tag), note["tags"]))
-
-    return note
-
 
 if db_provider == "elasticsearch":
     from db_providers.elastic import Db
@@ -112,9 +54,6 @@ def api_list(filter):
 
     notes = db.list(filter.lower(), sort)
 
-    for note in notes:
-        note = note_decode(note)
-
     return jsonify({"data": notes})
 
 
@@ -126,7 +65,7 @@ def api_note(uuid):
         if db_conn_err_persisting:
             return jsonify({"error": db_conn_err_persisting}), 503
 
-    note = note_decode(db.get(uuid))
+    note = db.get(uuid)
 
     return jsonify({"data": note})
 
@@ -139,11 +78,9 @@ def api_note_update(uuid):
 
     updated_note = json.loads(request.data)["note"]
     updated_note["updated"] = seconds
-    updated_note = note_encode(updated_note)
 
     db.update(updated_note["id"], updated_note)
 
-    updated_note = note_decode(updated_note)
     return jsonify({"data": updated_note})
 
 
@@ -163,11 +100,7 @@ def api_note_new():
     new_note["created"] = seconds
     new_note["updated"] = seconds
 
-    new_note = note_encode(new_note)
-
     db.create(new_note["id"], new_note)
-
-    new_note = note_decode(new_note)
 
     return jsonify({"data": new_note})
 
@@ -182,7 +115,7 @@ def api_tags(query):
             return jsonify({"error": db_conn_err_persisting}), 503
 
     # list(set()) removes duplicates from list
-    tags = list(set(list(map(lambda tag: string_decode(tag), db.tags()))))
+    tags = list(set(db.tags()))
 
     # filtering tags
     if len(query) > 0:
