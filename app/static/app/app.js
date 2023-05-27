c3b663d4be9cd62106bcdd3d2ca38403cffc70d1
Piotr Boruc <piotr.m.boruc@gmail.com>
2023-05-27 21:49:44 +0200

    deps update + move to CDN

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -3,39 +3,44 @@
 
 var app = angular.module("app", ['ui.router', 'ngSanitize', 'growlNotifications', 'ngTagsInput', 'ngclipboard'])
 
-.config([function() {
-  var renderer = new marked.Renderer();
+  .config([function () {
+    var renderer = new marked.Renderer();
 
-  // opening links in new tab (default link renderer override)
-  renderer.link = function(href, title, text) {
-    var link = marked.Renderer.prototype.link.call(this, href, title, text);
-    return link.replace("<a","<a target='_blank' ");
-  };
+    // opening links in new tab (default link renderer override)
+    renderer.link = function (href, title, text) {
+      var link = marked.Renderer.prototype.link.call(this, href, title, text);
+      return link.replace("<a", "<a target='_blank' ");
+    };
 
-  marked.setOptions({
+    marked.setOptions({
       renderer: renderer,
       gfm: true,
       tables: true,
       breaks: true,
       pedantic: false,
-      sanitizer: DOMPurify.sanitize,
       smartLists: true,
-      smartypants: false,
-      highlight: function (code, lang) {
+      mangle: false,
+      headerIds: false,
+      smartypants: false
+    });
+
+    marked.use(markedHighlight.markedHighlight({
+      langPrefix: 'language-',
+      highlight(code, lang) {
         if (lang) {
-          return hljs.highlight(lang, code).value;
+          return hljs.highlight(code, { language: lang }).value;
         } else {
           return hljs.highlightAuto(code).value;
         }
       }
-    });
-}])
+    }));
+  }])
 
-.config(["$locationProvider", function($locationProvider) {
-  $locationProvider.html5Mode(true);
-}])
+  .config(["$locationProvider", function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+  }])
 
-.run(['$rootScope', function($rootScope) {
-  $rootScope.notifications = [];
-}])
-;
+  .run(['$rootScope', function ($rootScope) {
+    $rootScope.notifications = [];
+  }])
+  ;
